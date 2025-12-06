dc0b0b434ed16b98d8b7a214f8c8df4526f19b1a
Cesar Gutierrez <ovedgu@hotmail.com>
2025-12-06 15:50:38 -0500

    punto 2 del proyecto

--- gendiff.js
+++ gendiff.js
@@ -4,8 +4,19 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .helpOption('-h, --help', 'output usage information');
+  .argument('<filepath1>', 'first config file')
+  .argument('<filepath2>', 'second config file')
+  .option('-f, --format <type>', 'output format')
+  .helpOption('-h, --help', 'output usage information')
+  .action((filepath1, filepath2, options) => {
+    // Acción temporal para probar que funciona
+    console.log('File 1:', filepath1);
+    console.log('File 2:', filepath2);
+    console.log('Format:', options.format);
+  });
+
 
 program.parse();
