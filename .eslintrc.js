d4648ff994ba30394b38cc9df998c8b4c507156b
Cesar Gutierrez <ovedgu@hotmail.com>
2025-12-15 16:14:24 -0500

    prueba 2 del CI

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+export default {
+  env: {
+    node: true,
+    es2022: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {
+    'no-console': 'off',
+  },
+};
---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,17 @@
+import path from 'path';
+import fs from 'fs';
+import genDiff from '../src/genDiff.js';
+
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
+
+const readFixture = (filename) =>
+  fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('gendiff flat json', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  const expected = readFixture('expected.txt');
+
+  expect(genDiff(filepath1, filepath2)).toBe(expected.trim());
+});
